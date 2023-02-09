0214c84a8fc0af255787fa2642ace1a829807c67
pushpamathi <pushpamathijeyam@gmail.com>
2023-02-09 08:25:42 +0530

    new repo

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,28 @@
+package org.lang;
+
+public class LanguageInfo extends StateDetails {
+	private void tamilLanguage() {
+		System.out.println("tamil");
+	}
+
+	private void englishLanguage() {
+		System.out.println("english");
+
+	}
+
+	private void hindiLanguage() {
+		System.out.println("hindi");
+
+	}
+
+	public static void main(String[] args) {
+		LanguageInfo l = new LanguageInfo();
+		l.tamilLanguage();
+		l.englishLanguage();
+		l.hindiLanguage();
+		l.southIndia();
+		l.northIndia();
+
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,12 @@
+package org.lang;
+
+public class StateDetails {
+	public void southIndia() {
+		System.out.println("south india");
+	}
+
+	public void northIndia() {
+		System.out.println("north india");
+	}
+
+}
